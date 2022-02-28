7e42d38d9ee2fe7878e36e62745802f4638d61c1
Garrett Girod <girodg@hotmail.com>
2022-02-28 12:00:24 -0500

    ref and out params

--- Day01/Day01/Program.cs
+++ Day01/Day01/Program.cs
@@ -8,12 +8,60 @@ namespace Day01
         {
             PrintMessage();
             string msg = GetMessage();
+            PrintMessage(msg);
+            TimeStamp(ref msg);
+            PrintMessage(msg);
+            int num1 = 10, num2 = 50;
+            int result = Sum(num1, num2);
+
+            int salary = 10000;
+            Factor(ref salary, (float)1.05);
+            Console.WriteLine(salary);
+
+            Console.WriteLine(DateTime.Now);
+            int grade = 59;
+            Factor(ref grade, 1.05F);
+
+            CurveGrade(grade, 5, out int newGrade);
+            Console.WriteLine($"{grade} was curved to {newGrade}.");
             Console.ReadKey();
+        }
+
+        static void CurveGrade(int grade, int curve, out int curvedGrade)
+        {
+            //curvedGrade = grade + curve;
+            //if (curvedGrade > 100) curvedGrade = 100;
+
+            //ternary operator -- shorthand for if-else
+            curvedGrade = (grade + curve > 100) ? 100 : grade + curve;
+        }
+
+        static void TimeStamp(ref string messageToTimestamp)
+        {
+            //$ - interpolated string
+            messageToTimestamp = $"{DateTime.Now}: {messageToTimestamp}";
+        }
+
+        static void Factor(ref int numberToChange, float factor)
+        {
+            numberToChange = (int)(numberToChange * factor);
+        }
+
+        static int Sum(int n1, int n2)
+        {
+            n1 += 10;//will this change num1? NO. it's just a copy.
+            return n1 + n2;
         }
 
         static void PrintMessage()
         {
-            Console.WriteLine("Batman is the best! Spider-who?");
+            string myMessage = "Batman is the best! Spider-who?";
+            Console.WriteLine(myMessage);
+        }
+
+        static void PrintMessage(string myMessage)
+        {
+            Console.WriteLine(myMessage);
         }
 
         static string GetMessage()
